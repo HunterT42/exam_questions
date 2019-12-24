498b473d2c066695b9965f5fd7fe188aa09c9d8a
Ilya Chernov <hunters7112@gmail.com>
2019-12-24 21:09:15 +0300

    update

--- site/scripts.js
+++ site/scripts.js
@@ -1,1 +1,8 @@
-console.log(2+2);
+function getRandomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+console.log('Номер первого вопроса: '+ getRandomInRange(1, 16));
+console.log('Номер второго вопроса: '+ getRandomInRange(1, 10));
+console.log('Номер третьего вопроса: '+ getRandomInRange(1, 10));
