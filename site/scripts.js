8cbd7290e480439c4b92d17ed755c39c6ff82f8c
Ilya Chernov <hunters7112@gmail.com>
2019-12-25 17:57:43 +0300

    upd

--- site/scripts.js
+++ site/scripts.js
@@ -1,8 +1,10 @@
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
+console.log(getRandomInRange(1, 10));
+console.log('Done!');
 
-
-console.log('Номер первого вопроса: '+ getRandomInRange(1, 16));
-console.log('Номер второго вопроса: '+ getRandomInRange(1, 10));
-console.log('Номер третьего вопроса: '+ getRandomInRange(1, 10));
+function get (){
+console.log(getRandomInRange(1, 10));
+};
+kek.onclick = get;
