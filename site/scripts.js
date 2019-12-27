b040bfda2b37a2bd67bf106c238c8b5ba36abf10
Ilya Chernov <hunters7112@gmail.com>
2019-12-27 18:37:05 +0300

    upd

--- site/scripts.js
+++ site/scripts.js
@@ -19,204 +19,241 @@ var categories = [{
 var all_questions = [{
     index: 0,
     category: 0,
-    name: 'Развитие языков программирования. Обзор языков программирования. Области применения языков программирования.'
+    name: '1.Развитие языков программирования. Обзор языков программирования. Области применения языков программирования.'
   },
   {
     index: 1,
     category: 0,
-    name: 'Компиляторы и интерпретаторы. Жизненный цикл программы.'
+    name: '2.Компиляторы и интерпретаторы. Жизненный цикл программы.'
   },
   {
     index: 2,
     category: 0,
-    name: 'Программа. Программный продукт и его характеристики. Основные этапы решения задач на компьютере.'
+    name: '3.Программа. Программный продукт и его характеристики. Основные этапы решения задач на компьютере.'
   },
   {
     index: 3,
     category: 0,
-    name: 'Переменные и константы. Объявление объектов данных. Внутреннее представление данных в памяти компьютера.'
+    name: '4.Переменные и константы. Объявление объектов данных. Внутреннее представление данных в памяти компьютера.'
   },
   {
     index: 4,
     category: 0,
-    name: 'Типы данных. Простые типы данных.'
+    name: '5.Типы данных. Простые типы данных.'
   },
   {
     index: 5,
     category: 0,
-    name: 'Производные типы данных. Структурированные типы данных.'
+    name: '6.Производные типы данных. Структурированные типы данных.'
   },
   {
     index: 6,
     category: 0,
-    name: 'Операции и выражения. Правила формирования и вычисления выражений. Структура программы. Ввод и вывод данных.'
+    name: '7.Операции и выражения. Правила формирования и вычисления выражений. Структура программы. Ввод и вывод данных.'
   },
   {
     index: 7,
     category: 0,
-    name: 'Оператор присваивания. Составной оператор. Условный оператор. Оператор выбора.'
+    name: '8.Оператор присваивания. Составной оператор. Условный оператор. Оператор выбора.'
   },
   {
     index: 8,
     category: 0,
-    name: 'Понятие условия. Виды условий. Комбинирование различных условий.'
+    name: '9.Понятие условия. Виды условий. Комбинирование различных условий.'
   },
   {
     index: 9,
     category: 0,
-    name: 'Понятие цикла. Виды циклов. Цикл с постусловием. Цикл с предусловием. Цикл с параметром. Вложенные циклы.'
+    name: '10.Понятие цикла. Виды циклов. Цикл с постусловием. Цикл с предусловием. Цикл с параметром. Вложенные циклы.'
   },
   {
     index: 10,
     category: 0,
-    name: 'Понятие рекурсии. Пример рекурсии. Отличие рекурсии от вложенных циклов.'
+    name: '11.Понятие рекурсии. Пример рекурсии. Отличие рекурсии от вложенных циклов.'
   },
   {
     index: 11,
     category: 0,
-    name: 'Функции и методы в программировании. Понятие аргумента. Примеры функций с аргументами и без. Понятие чистой функции.'
+    name: '12.Функции и методы в программировании. Понятие аргумента. Примеры функций с аргументами и без. Понятие чистой функции.'
   },
   {
     index: 12,
     category: 0,
-    name: 'Понятие потока выполнения программы. Понятие прерываний. Влияние прерываний на поток выполнения.'
+    name: '13.Понятие потока выполнения программы. Понятие прерываний. Влияние прерываний на поток выполнения.'
   },
   {
     index: 13,
     category: 0,
-    name: 'Понятие инкремента и декремента. Примеры инкрементации и декрементации значений переменных в программировании.'
+    name: '14.Понятие инкремента и декремента. Примеры инкрементации и декрементации значений переменных в программировании.'
   },
   {
     index: 14,
     category: 0,
-    name: 'Ошибки в программировании. Виды ошибок. Способы отладки.'
+    name: '15.Ошибки в программировании. Виды ошибок. Способы отладки.'
   },
   {
     index: 15,
     category: 0,
-    name: 'Итеративный процесс. Понятие итерации. Примеры итераций.'
+    name: '16.Итеративный процесс. Понятие итерации. Примеры итераций.'
   },
   {
     index: 16,
     category: 1,
-    name: 'Язык JavaScript, история его возникновения. Особенности языка.'
+    name: '1.Язык JavaScript, история его возникновения. Особенности языка.'
   },
   {
     index: 17,
     category: 1,
-    name: 'Что может и чего не может JavaScript в браузере. Какие существую варианты обхода этих ограничений.'
+    name: '2.Что может и чего не может JavaScript в браузере. Какие существую варианты обхода этих ограничений.'
   },
   {
     index: 18,
     category: 1,
-    name: 'Понятие скрипт. Написание и исполнение скриптов на JavaScript. Способы интерпретации скриптов, написанных на JavaScript.'
+    name: '3.Понятие скрипт. Написание и исполнение скриптов на JavaScript. Способы интерпретации скриптов, написанных на JavaScript.'
   },
   {
     index: 19,
     category: 1,
-    name: 'Переменные и постоянные (константы) в JavaScript. Способы их задать.'
+    name: '4.Переменные и постоянные (константы) в JavaScript. Способы их задать.'
   },
   {
     index: 20,
     category: 1,
-    name: 'Ввод и вывод в JavaScript. Как можно вводить данные и куда выводить.'
+    name: '5.Ввод и вывод в JavaScript. Как можно вводить данные и куда выводить.'
   },
   {
     index: 21,
     category: 1,
-    name: 'Типы данных в JavaScript. Особенности типов данных в JavaScript в отличие от большинства других популярных языков программирования. Преобразование (приведение) типов.'
+    name: '6.Типы данных в JavaScript. Особенности типов данных в JavaScript в отличие от большинства других популярных языков программирования. Преобразование (приведение) типов.'
   },
   {
     index: 22,
     category: 1,
-    name: 'Условия в JavaScript, их виды и способы их задать. Вложенные условия, примеры на JavaScript.'
+    name: '7.Условия в JavaScript, их виды и способы их задать. Вложенные условия, примеры на JavaScript.'
   },
   {
     index: 23,
     category: 1,
-    name: 'Циклы в JavaScript, их основные виды и назначение. Отличие вложенного цикла и рекурсии, примеры на JavaScript.'
+    name: '8.Циклы в JavaScript, их основные виды и назначение. Отличие вложенного цикла и рекурсии, примеры на JavaScript.'
   },
   {
     index: 24,
     category: 1,
-    name: 'Функции в JavaScript, виды функций. Назначение функций в JavaScript. Примеры функций на JavaScript.'
+    name: '9.Функции в JavaScript, виды функций. Назначение функций в JavaScript. Примеры функций на JavaScript.'
   },
   {
     index: 25,
     category: 1,
-    name: 'Способы отладки кода на JavaScript на примере браузера Google Chrome.'
+    name: '10.Способы отладки кода на JavaScript на примере браузера Google Chrome.'
   },
   {
     index: 26,
     category: 2,
-    name: 'Язык JavaScript, история его возникновения. Особенности языка.'
+    name: '1.Язык JavaScript, история его возникновения. Особенности языка.'
   },
   {
     index: 27,
     category: 2,
-    name: 'Что может и чего не может JavaScript в браузере. Какие существую варианты обхода этих ограничений.'
+    name: '2.Что может и чего не может JavaScript в браузере. Какие существую варианты обхода этих ограничений.'
   },
   {
     index: 28,
     category: 2,
-    name: 'Понятие скрипт. Написание и исполнение скриптов на JavaScript. Способы интерпретации скриптов, написанных на JavaScript.'
+    name: '3.Понятие скрипт. Написание и исполнение скриптов на JavaScript. Способы интерпретации скриптов, написанных на JavaScript.'
   },
   {
     index: 29,
     category: 2,
-    name: 'Переменные и постоянные (константы) в JavaScript. Способы их задать.'
+    name: '4.Переменные и постоянные (константы) в JavaScript. Способы их задать.'
   },
   {
     index: 30,
     category: 2,
-    name: 'Ввод и вывод в JavaScript. Как можно вводить данные и куда выводить.'
+    name: '5.Ввод и вывод в JavaScript. Как можно вводить данные и куда выводить.'
   },
   {
     index: 31,
     category: 2,
-    name: 'Типы данных в JavaScript. Особенности типов данных в JavaScript в отличие от большинства других популярных языков программирования. Преобразование (приведение) типов.'
+    name: '6.Типы данных в JavaScript. Особенности типов данных в JavaScript в отличие от большинства других популярных языков программирования. Преобразование (приведение) типов.'
   },
   {
     index: 32,
     category: 2,
-    name: 'Условия в JavaScript, их виды и способы их задать. Вложенные условия, примеры на JavaScript.'
+    name: '7.Условия в JavaScript, их виды и способы их задать. Вложенные условия, примеры на JavaScript.'
   },
   {
     index: 33,
     category: 2,
-    name: 'Циклы в JavaScript, их основные виды и назначение. Отличие вложенного цикла и рекурсии, примеры на JavaScript.'
+    name: '8.Циклы в JavaScript, их основные виды и назначение. Отличие вложенного цикла и рекурсии, примеры на JavaScript.'
   },
   {
     index: 34,
     category: 2,
-    name: 'Функции в JavaScript, виды функций. Назначение функций в JavaScript. Примеры функций на JavaScript.'
+    name: '9.Функции в JavaScript, виды функций. Назначение функций в JavaScript. Примеры функций на JavaScript.'
   },
   {
     index: 35,
     category: 2,
-    name: 'Способы отладки кода на JavaScript на примере браузера Google Chrome.'
+    name: '10.Способы отладки кода на JavaScript на примере браузера Google Chrome.'
   }
 ];
 
-// var firstQuestion = function (all_questions[].category){
-//
-// };
+var questionOne = [];
+var questionTwo = [];
+var questionThree = [];
 
 for (var i = 0; i < all_questions.length; i++) {
   if (all_questions[i].category == 0) {
-    var kek = all_questions[i];
-    console.log(kek);
+    questionOne.push(all_questions[i]);
+  }
+}
+for (var i = 0; i < all_questions.length; i++) {
+  if (all_questions[i].category == 1) {
+     questionTwo.push(all_questions[i]);
+  }
+}
+for (var i = 0; i < all_questions.length; i++) {
+  if (all_questions[i].category == 2) {
+     questionThree.push(all_questions[i]);
   }
 }
 
+var encodedMessageOne = [ ];
+encodedMessageOne.push(getRandomInRange(0, 15));
+
+var decodedMessageOne = '';
+
+for(var i = 0; i <= encodedMessageOne.length - 1; i++){
+  decodedMessageOne += questionOne[encodedMessageOne[i]].name;
+  }
 
 
+var encodedMessageTwo = [ ];
+encodedMessageTwo.push(getRandomInRange(0, 9));
+
+var decodedMessageTwo = '';
+
+for(var i = 0; i <= encodedMessageTwo.length - 1; i++){
+  decodedMessageTwo += questionTwo[encodedMessageTwo[i]].name;
+  }
 
 
+var encodedMessageThree = [ ];
+encodedMessageThree.push(getRandomInRange(0, 9));
+
+var decodedMessageThree = '';
+
+for(var i = 0; i <= encodedMessageThree.length - 1; i++){
+  decodedMessageThree += questionThree[encodedMessageThree[i]].name;
+  }
 
 
+kek.onclick = lasagna;
 
-
+function lasagna (){
+    var el = document.querySelector('.all_questions');
+    el.innerHTML = '<p class="boldp">' + 'Первый вопрос:<br>' + '</p>' + '<p>' + decodedMessageOne + '</p>' + '<p class="boldp">' + '<br>Второй вопрос:<br>' + '</p>' + '<p>' + decodedMessageTwo + '</p>' +'<p class="boldp">' + '<br>Третий вопрос:<br>'+ '</p>'+ '<p>' + decodedMessageThree + '</p>';
+};
 // //1
 // var questionOne = [
 //  '0',
@@ -306,16 +343,10 @@ for (var i = 0; i < all_questions.length; i++) {
 //   }
 // console.log(decodedMessageThree);
 //
-// kek.onclick = lasagna;
+
 //
-//
-// function lasagna (){
-//     var el = document.querySelector('.all_questions');
-//     el.innerHTML = '<p class="boldp">' + 'Первый вопрос:<br>' + '</p>' + '<p>' + decodedMessageOne + '</p>' + '<p class="boldp">' + '<br>Второй вопрос:<br>' + '</p>' + '<p>' + decodedMessageTwo + '</p>' +'<p class="boldp">' + '<br>Третий вопрос:<br>'+ '</p>'+ '<p>' + decodedMessageThree + '</p>';
-// };
-//
-// kekus.onclick = reload;
-//
-// function reload () {
-//   location.reload();
-// };
+kekus.onclick = reload;
+
+function reload () {
+  location.reload();
+};
